7b1c5aead61d76acd15f002db6d3b04214deba1b
cj-shu <cj1125@shu.edu.cn>
2021-05-11 18:57:33 +0800

    TIPD code

---
+++ ESC_main.R
@@ -0,0 +1,171 @@
+#2717 mouse embryonic stem cells£¬with 24174 observed unique transcripts
+library(edgeR)
+library(DESeq)
+library(ggplot2)
+library(igraph)
+library(slingshot)
+library(Rtsne)
+# library(umap)
+library(cluster)
+library(seewave)
+library(factoextra)
+library(mclust)
+setwd("C:/Users/Documents/R_projects/mouse_ESC")
+
+data_d0<-read.table("GSM1599494_ES_d0_main.csv",sep=",",header=T,row.names=1)
+data_d2<-read.table("GSM1599497_ES_d2_LIFminus.csv",sep=",",header=T,row.names=1)
+data_d4<-read.table("GSM1599498_ES_d4_LIFminus.csv",sep=",",header=T,row.names=1)
+data_d7<-read.table("GSM1599499_ES_d7_LIFminus.csv",sep=",",header=T,row.names=1)
+
+colnames(data_d0)<-paste("d0_",colnames(data_d0),sep="")
+colnames(data_d2)<-paste("d2_",colnames(data_d2),sep="")
+colnames(data_d4)<-paste("d4_",colnames(data_d4),sep="")
+colnames(data_d7)<-paste("d7_",colnames(data_d7),sep="")
+
+mouse_ESC_data<-cbind(data_d0,data_d2,data_d4,data_d7)
+mouse_ESC_data<- mouse_ESC_data[rowMeans(mouse_ESC_data > 1) > 0.3,]
+
+#CPM normalization
+group = c(rep("d0",length(data_d0)),rep("d2",length(data_d2)),rep("d4",length(data_d4)),rep("d7",length(data_d7)))
+dge <- DGEList(
+  counts = mouse_ESC_data, 
+ norm.factors = rep(1, length(mouse_ESC_data[1,])), 
+  group = group
+)
+# dge<-calcNormFactors(dge)
+group_edgeR <- factor(group)
+design <- model.matrix(~ group_edgeR)
+dge <- estimateDisp(dge, design = design, trend.method = "none")
+data_cpm = cpm(dge)
+write.table(data_cpm,"forSRfilter.txt")
+log_data_cpm<-log2(data_cpm+1)
+write.table(log_data_cpm,"filter_data.txt")
+
+## dimensionality reduction
+filter_data<-read.table("filter_data.txt",row.names = 1)
+data<-filter_data
+tsne_data<-Rtsne(t(as.matrix(data)),pca = T,theta = 0.0)
+plot(tsne_data$Y)
+tsne_data<-as.data.frame(tsne_data$Y)
+rownames(tsne_data)<-colnames(data)
+colnames(tsne_data)<-c("PC1","PC2")
+
+#umap
+# umap_data<-umap(t(as.matrix(data)))
+# umap_data<-as.data.frame(umap_data$layout)
+# plot(umap_data)
+# rownames(umap_data)<-colnames(data)
+# colnames(umap_data)<-c("PC1","PC2")
+##clustering
+gap_stat<-clusGap(tsne_data,kmeans,K.max = 8,B=200,verbose=interactive())
+best_k<-maxSE(f = gap_stat$Tab[, "gap"], SE.f = gap_stat$Tab[, "SE.sim"])
+# #fviz_gap_stat(gap_stat)
+clustered_data<-kmeans(tsne_data,best_k)
+cluster_result<-clustered_data$cluster
+
+## compute potency states
+ESC_SR<-read.table("ESC_SR.txt")
+rownames(ESC_SR)<-colnames(filter_data)
+colnames(ESC_SR)<-c("sample_name","SR")
+
+infer_SR_states<-function(logSR,max_state_num=3)
+{
+  print("Fit Gaussian Mixture Model to Signaling Entropies")
+  model.o=Mclust(logSR,G=seq_len(max_state_num))  
+  potS <- model.o$classification
+  nPS <- length(levels(as.factor(potS)))
+  print(paste("Identified ",nPS," potency states",sep=""))
+  #plot(model.o,what = "classification")
+  return(model.o)
+}
+logSR<-log2(ESC_SR$SR/(1-ESC_SR$SR))
+model.o<-infer_SR_states(logSR)
+table(model.o$classification)
+
+#The probability distribution of heterogeneous states in each cell cluster was calculated
+cluster_result<-as.data.frame(cluster_result)
+cluster_result$cellid<-rownames(cluster_result)
+km_SRstates<-cbind(cluster_result,model.o$classification)
+colnames(km_SRstates)<-c("cluster","cellid","states")
+print(best_k)
+#cluster1
+# cons=0.0001
+cluster1_freq1=nrow(km_SRstates[which(km_SRstates$cluster==1&km_SRstates$states==1),])/length(km_SRstates[which(km_SRstates$cluster==1),]$cluster)
+cluster1_freq2=nrow(km_SRstates[which(km_SRstates$cluster==1&km_SRstates$states==2),])/length(km_SRstates[which(km_SRstates$cluster==1),]$cluster)
+cluster1_freq3=nrow(km_SRstates[which(km_SRstates$cluster==1&km_SRstates$states==3),])/length(km_SRstates[which(km_SRstates$cluster==1),]$cluster)
+clu1_distrib<-data.frame(states<-c(1:3),freq<-c(cluster1_freq1,cluster1_freq2,cluster1_freq3))
+colnames(clu1_distrib)<-c("states","freq")
+print(clu1_distrib)
+#cluster2
+cluster2_freq1=nrow(km_SRstates[which(km_SRstates$cluster==2&km_SRstates$states==1),])/length(km_SRstates[which(km_SRstates$cluster==2),]$cluster)
+cluster2_freq2=nrow(km_SRstates[which(km_SRstates$cluster==2&km_SRstates$states==2),])/length(km_SRstates[which(km_SRstates$cluster==2),]$cluster)
+cluster2_freq3=nrow(km_SRstates[which(km_SRstates$cluster==2&km_SRstates$states==3),])/length(km_SRstates[which(km_SRstates$cluster==2),]$cluster)
+clu2_distrib<-data.frame(states<-c(1:3),freq<-c(cluster2_freq1,cluster2_freq2,cluster2_freq3))
+colnames(clu2_distrib)<-c("states","freq")
+print(clu2_distrib)
+#cluster3
+cluster3_freq1=nrow(km_SRstates[which(km_SRstates$cluster==3&km_SRstates$states==1),])/length(km_SRstates[which(km_SRstates$cluster==3),]$cluster)
+cluster3_freq2=nrow(km_SRstates[which(km_SRstates$cluster==3&km_SRstates$states==2),])/length(km_SRstates[which(km_SRstates$cluster==3),]$cluster)
+cluster3_freq3=nrow(km_SRstates[which(km_SRstates$cluster==3&km_SRstates$states==3),])/length(km_SRstates[which(km_SRstates$cluster==3),]$cluster)
+clu3_distrib<-data.frame(states<-c(1:3),freq<-c(cluster3_freq1,cluster3_freq2,cluster3_freq3))
+colnames(clu3_distrib)<-c("states","freq")
+print(clu3_distrib)
+
+all_distrib<-list(as.matrix(clu1_distrib),as.matrix(clu2_distrib)
+                  ,as.matrix(clu3_distrib)
+)
+#Calculate the JS divergence distance
+JS_compute<-function(prob_distrib1,prob_distrib2){
+  prob1=prob_distrib1[,2]
+  prob2=prob_distrib2[,2]
+  M=(prob1+prob2)/2
+  return(0.5*(kl.dist(prob1,M,2)$D1)+0.5*(kl.dist(prob2,M,2)$D1))
+}
+JS_dist=matrix(0,nrow=length(all_distrib),ncol=length(all_distrib))
+for (i in seq_len(length(levels(as.factor(km_SRstates$cluster))))) {
+  for(j in seq_len(length(levels(as.factor(km_SRstates$cluster))))){
+    JS_dist[i,j]=JS_compute(all_distrib[[i]],all_distrib[[j]])
+  }
+}
+
+merge_data<-cbind(ESC_SR,cluster_result)
+merge_data<-merge_data[,c(1,2,3)]
+colnames(merge_data)<-c("cellid","SR","cluster")
+#write.csv(merge_data,"SR_cluster.csv")
+avgSR_cluster1<-mean(merge_data[which(merge_data$cluster=="1"),]$SR)
+avgSR_cluster2<-mean(merge_data[which(merge_data$cluster=="2"),]$SR)
+avgSR_cluster3<-mean(merge_data[which(merge_data$cluster=="3"),]$SR)
+
+avgSR_cluster<-data.frame(avgSR_cluster1,avgSR_cluster2,avgSR_cluster3)
+start_cluster<-colnames(avgSR_cluster)[which(avgSR_cluster==max(avgSR_cluster),arr.ind = T)[,2]]
+#write.table(avgSR_cluster,"averageSR_cluster.txt",col.names = F)
+##########################################################
+#######compute MST
+##########################################################
+# reduceDimdata_matrix=read.table("tsne_reducedata.txt",row.names = 1)
+reduceDimdata_matrix=as.matrix(tsne_data)
+JS_dist<-as.matrix(JS_dist)
+gp <- graph.adjacency(JS_dist, mode = "undirected", weighted = TRUE)
+dp_mst <- minimum.spanning.tree(gp) 
+print(dp_mst)
+###########################################################
+###########Construct the simultaneous curves and compute pseudotime values
+###########################################################
+clusLabel=cluster_result$cluster_result
+clusLabel=t(clusLabel)
+clusLabel=as.numeric(clusLabel)
+names(clusLabel)<-cluster_result$cellid
+lineage<-getLineages(reduceDimdata_matrix,clusLabel)
+lineage_list=list(Lineage1=c("3","1","2"))
+#Update the parameters of slingparams
+params=slingParams(lineage)
+slingParams=params
+slingParams$start.clus<-"3"
+slingParams$end.clus<-"2"
+slingParams$end.given<-FALSE
+inputofcurves<-newSlingshotDataSet(reducedDim = reduceDimdata_matrix,clusterLabels = clusLabel,lineages=lineage_list,adjacency=JS_dist,curves=list(),slingParams=slingParams)
+outofcurves<-getCurves(inputofcurves,extend="n")
+pseudotime<-outofcurves@curves$curve1$lambda
+pseudotime.sort<-sort(pseudotime,decreasing = F)
+write.table(pseudotime,"ESC_pseudotime.txt")
+
---
+++ mo_ESC_SR_compute.R
@@ -0,0 +1,271 @@
+#mouse ESC 2717 samples
+#SR compute
+library(clusterProfiler)
+library(org.Mm.eg.db)#mouse
+library(stringr)
+library(Matrix)
+library(base)
+library(AnnotationDbi)
+library(igraph)
+library(ggplot2)
+setwd("C:/Users/yyt/Documents/R_projects/mouse_ESC")
+options(stringsAsFactors = FALSE) 
+protein_interactions<-read.table("10090.protein.links.v11.0.txt",header = T)
+#idmapping<-read.table("HUMAN_9606_idmapping_selected.tab",header=F,as.is=T,sep="\t")
+filter_data<-read.table("filter_data.txt",header=T)
+geneid<-rownames(filter_data)
+
+gene_symbol_to_ensp<-select(org.Mm.eg.db,keys=geneid,column="ENSEMBLPROT",keytype="SYMBOL")
+removeNAindex<-which(is.na(gene_symbol_to_ensp[,2]))
+gene_symbol_to_ensp<-gene_symbol_to_ensp[-removeNAindex,]
+protein_interactions$protein1<-str_replace(protein_interactions$protein1,"10090.","")
+protein_interactions$protein2<-str_replace(protein_interactions$protein2,"10090.","")
+protein_interactions<-protein_interactions[which(protein_interactions[,1] %in% gene_symbol_to_ensp[,2] & protein_interactions[,2] %in% gene_symbol_to_ensp[,2]),]
+# remove duplicates
+protein_interactions$identical<-paste0(protein_interactions[,1],protein_interactions[,2])
+protein_interactions<-protein_interactions[!duplicated(protein_interactions$identical),]
+protein_interactions$identical<-paste0(protein_interactions[,2],protein_interactions[,1])
+protein_interactions<-protein_interactions[!duplicated(protein_interactions$identical),]
+ppi_network<-protein_interactions[,c(1,2)]
+write.table(ppi_network,"ppi_network.txt")
+
+forSRfilter<-read.table("forSRfilter.txt",header=T,row.names = 1)#Gene expression data that has been gene-filtered but not logarithmic
+ppi_network<-read.table("ppi_network.txt",header=T)
+forSRfilter<-forSRfilter[which(rownames(forSRfilter) %in% geneid),]
+#ensp tranfer to symbol
+ppi_network1<-as.character(ppi_network[,1])
+ppi_network2<-as.character(ppi_network[,2])
+annot1<-select(org.Mm.eg.db,keys=ppi_network1,columns="SYMBOL",keytype="ENSEMBLPROT")
+annot2<-select(org.Mm.eg.db,keys=ppi_network2,columns="SYMBOL",keytype="ENSEMBLPROT")
+ppi_network<-cbind(annot1,annot2)
+ppi_network<-ppi_network[,c(2,4)]
+removeNAindex1<-which(is.na(ppi_network[,1]))
+removeNAindex2<-which(is.na(ppi_network[,2]))
+
+edgelist = ppi_network[,c(1,2)]
+write.csv(edgelist,file="edgelist.csv",row.names = F)
+node_union<-union(ppi_network[,1],ppi_network[,2])
+
+genes<-rownames(forSRfilter)
+SR_filter_data<-forSRfilter[which(genes %in% node_union),]
+setdiff(node_union,rownames(SR_filter_data))
+node_union=union(edgelist[,1],edgelist[,2])
+setdiff(node_union,rownames(SR_filter_data))
+removelist<-c(setdiff(node_union,rownames(SR_filter_data)))
+index1<-which(edgelist[,1] %in% removelist)
+index2<-which(edgelist[,2] %in% removelist)
+index<-c(index1,index2)
+index<-unique(index)
+edgelist<-edgelist[-index,]
+node_union=union(edgelist[,1],edgelist[,2])
+setdiff(node_union,rownames(SR_filter_data))
+g1<-graph.data.frame(edgelist,directed=F)
+g2<-get.adjacency(g1,sparse=FALSE)
+
+SR_filter_data<forSRfilter[which(rownames(forSRfilter) %in% rownames(SR_filter_data)),]
+
+DoIntegPPI <- function(exp.m, 
+                       ppiA.m,
+                       log_trans = TRUE)
+{
+  # set input data matrix class
+  data.class <- class(exp.m)
+  
+  # select log-normalization methods based on data class
+  if (data.class == "SingleCellExperiment") {
+    sizeFactors(exp.m) <- scater::librarySizeFactors(exp.m)
+    exp.m <- scater::normalize(exp.m, log_exprs_offset = 1.1)
+    data.m <- Matrix::as.matrix(SummarizedExperiment::assay(exp.m, i = "logcounts"))
+  }else if (data.class == "CellDataSet") {
+    exp.m <- BiocGenerics::estimateSizeFactors(exp.m)
+    data.m <- Matrix::as.matrix(t(t(Biobase::exprs(exp.m)) / 
+                                    Biobase::pData(exp.m)[, 'Size_Factor']))
+    data.m <- log2(data.m + 1.1)
+  }else{
+    data.m <- as.matrix(exp.m)
+    if (log_trans) {
+      TRC.v <- colSums(exp.m)
+      maxC <- max(TRC.v)
+      for (i in seq_len(dim(data.m)[2])) {
+        temp <- maxC / TRC.v[i]
+        data.m[, i] <- log2(exp.m[, i] * temp + 1.1)
+      }
+    }
+  }
+  
+  if (min(data.m) == 0) {
+    stop("Input matrix must have non-zero minimal value, please set 
+         log_trans = TRUE!")
+  }
+  
+  commonEID.v <- intersect(rownames(ppiA.m),rownames(data.m))
+  
+  if (DelayedArray::isEmpty(commonEID.v) == TRUE) {
+    stop("scRNA-seq data should have the same gene identifier with the network!")
+  }
+  
+  match(commonEID.v,rownames(data.m)) -> map1.idx
+  expPIN.m <- data.m[map1.idx,]
+  
+  match(commonEID.v,rownames(ppiA.m)) -> map2.idx
+  adj.m <- ppiA.m[map2.idx,map2.idx]
+  
+  gr.o <- igraph::graph.adjacency(adj.m,mode="undirected")
+  comp.l <- igraph::clusters(gr.o)
+  cd.v <- summary(factor(comp.l$member))
+  mcID <- as.numeric(names(cd.v)[which.max(cd.v)])
+  maxc.idx <- which(comp.l$member==mcID)
+  adjMC.m <- adj.m[maxc.idx,maxc.idx]
+  expMC.m <- expPIN.m[maxc.idx,]
+  
+  if (data.class == "SingleCellExperiment") {
+    return(list(data.sce = exp.m, expMC = expMC.m, adjMC = adjMC.m, data = data.m))
+  }else if (data.class == "CellDataSet") {
+    return(list(data.cds = exp.m, expMC = expMC.m, adjMC = adjMC.m, data = data.m))
+  }else{
+    return(list(expMC = expMC.m, adjMC = adjMC.m, data = data.m))
+  }
+  }
+Integration.l <- DoIntegPPI(SR_filter_data,g2)
+
+CompSRana <- function(Integration.l,
+                      local = FALSE,
+                      mc.cores=1)
+{
+  ### compute maxSR for SR normalization
+  Integration.l <- CompMaxSR(Integration.l)
+  maxSR <- Integration.l$maxSR
+  
+  idx.l <- as.list(seq_len(ncol(Integration.l$expMC)))
+  out.l <- mclapply(idx.l, CompSRanaPRL, 
+                    exp.m=Integration.l$expMC, 
+                    adj.m=Integration.l$adjMC,
+                    local=local,
+                    maxSR=maxSR,
+                    mc.cores=mc.cores)
+  SR.v <- sapply(out.l, function(v) return(v[[1]]))
+  invP.v <- sapply(out.l, function(v) return(v[[2]]))
+  S.v <- sapply(out.l, function(v) return(v[[3]]))
+  NS.v <- sapply(out.l, function(v) return(v[[4]]))
+  
+  Integration.l$SR <- SR.v 
+  Integration.l$inv <- invP.v
+  Integration.l$s <- S.v
+  Integration.l$ns <- NS.v
+  
+  if (!is.null(Integration.l$data.sce)) {
+    colData(Integration.l$data.sce)$SR <- SR.v
+  }else if (!is.null(Integration.l$data.cds)) {
+    pData(Integration.l$data.cds)$SR <- SR.v
+  }
+  return(Integration.l)
+}
+
+CompSRana <- function(Integration.l,
+                      local = FALSE,
+                      mc.cores=1)
+{
+  ### compute maxSR for SR normalization
+  Integration.l <- CompMaxSR(Integration.l)
+  maxSR <- Integration.l$maxSR
+  
+  idx.l <- as.list(seq_len(ncol(Integration.l$expMC)))
+  out.l <- mclapply(idx.l, CompSRanaPRL, 
+                    exp.m=Integration.l$expMC, 
+                    adj.m=Integration.l$adjMC,
+                    local=local,
+                    maxSR=maxSR,
+                    mc.cores=mc.cores)
+  SR.v <- sapply(out.l, function(v) return(v[[1]]))
+  invP.v <- sapply(out.l, function(v) return(v[[2]]))
+  S.v <- sapply(out.l, function(v) return(v[[3]]))
+  NS.v <- sapply(out.l, function(v) return(v[[4]]))
+  
+  Integration.l$SR <- SR.v
+  Integration.l$inv <- invP.v
+  Integration.l$s <- S.v
+  Integration.l$ns <- NS.v
+  
+  if (!is.null(Integration.l$data.sce)) {
+    colData(Integration.l$data.sce)$SR <- SR.v
+  }else if (!is.null(Integration.l$data.cds)) {
+    pData(Integration.l$data.cds)$SR <- SR.v
+  }
+  return(Integration.l)
+}
+
+CompMaxSR <- function(Integration.l){
+  
+  adj.m <- Integration.l$adjMC
+  
+  # find right eigenvector of adjacency matrix
+  fa <- function(x,extra=NULL) {
+    as.vector(adj.m %*% x)
+  }
+  ap.o <- igraph::arpack(fa,options=list(n=nrow(adj.m),nev=1,which="LM"), sym=TRUE)
+  v <- ap.o$vectors
+  lambda <- ap.o$values
+  
+  # maximum entropy
+  MaxSR <- log(lambda)
+  
+  Integration.l$maxSR <- MaxSR
+  
+  return(Integration.l)
+}
+
+CompSRanaPRL <- function(idx,
+                         exp.m,
+                         adj.m,
+                         local=TRUE,
+                         maxSR=NULL)
+{
+  
+  # compute outgoing flux around each node
+  exp.v <- exp.m[,idx];
+  sumexp.v <- as.vector(adj.m %*% matrix(exp.v,ncol=1));
+  invP.v <- exp.v*sumexp.v;
+  nf <- sum(invP.v);
+  invP.v <- invP.v/nf;
+  p.m <- t(t(adj.m)*exp.v)/sumexp.v;
+  S.v <- apply(p.m,1,CompS);
+  SR <- sum(invP.v*S.v);
+  # if provided then normalise relative to maxSR
+  if(is.null(maxSR)==FALSE){
+    SR <- SR/maxSR;
+  }
+  if(local){
+    NS.v <- apply(p.m,1,CompNS);
+  }
+  else {
+    NS.v <- NULL;
+  }
+  return(list(sr=SR,inv=invP.v,s=S.v,ns=NS.v));
+}
+
+CompNS <- function(p.v){
+  
+  tmp.idx <- which(p.v>0);
+  if(length(tmp.idx)>1){
+    NLS <- -sum( p.v[tmp.idx]*log(p.v[tmp.idx]) )/log(length(tmp.idx));
+  }
+  else {
+    # one degree nodes have zero entropy, avoid singularity.
+    NLS <- 0;
+  }
+  return(NLS);
+}
+
+CompS <- function(p.v){
+  
+  tmp.idx <- which(p.v>0);
+  LS <-  - sum( p.v[tmp.idx]*log(p.v[tmp.idx]) )
+  return(LS);
+}
+Integration.l<-CompSRana(Integration.l)
+print(Integration.l$SR)
+summary(Integration.l$SR)
+
+SR<-as.data.frame(Integration.l$SR)
+rownames(SR)<-colnames(filter_data)
+write.table(SR,"ESC_SR.txt",quote=F,col.names = F)
